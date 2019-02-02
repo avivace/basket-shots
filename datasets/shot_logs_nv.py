d6dd7600053487d775ec20d786b8189df19906aa
luca <palazzi.luca18@gmail.com>
2019-02-02 17:36:13 +0100

    Add updated script Python for shot percentage. Add new dataset with new feature

--- datasets/shot_logs_nv.py
+++ datasets/shot_logs_nv.py
@@ -8,9 +8,13 @@ import pandas as pd
 
 data = pd.read_csv(r"C:\Users\luca\Documents\GitHub\basket-shots\datasets\shot_logs.csv")
 df = data.sort_values(by=['player_name', 'GAME_ID'])
-df_agg = df.groupby(['player_name', 'GAME_ID']).agg({'SHOT_NUMBER':'max', 'FGM':'sum'})
-df_agg = df_agg.reset_index()
-df_shift = df_agg.groupby('player_name')['SHOT_NUMBER', 'FGM'].shift(1)
+df_agg = df.groupby(['player_name', 'GAME_ID']).agg({'SHOT_NUMBER':'max', 'FGM':'sum'}).reset_index()
+df_cs = df_agg.groupby('player_name').agg({'SHOT_NUMBER':'cumsum', 'FGM':'cumsum'})
 df_agg = df_agg.drop(columns=['SHOT_NUMBER', 'FGM'])
+df_agg = df_agg.join(df_cs)
+df_agg['% Previous'] = round(df_agg['FGM'] / df_agg['SHOT_NUMBER'], 2)
+df_shift = df_agg.groupby('player_name')['% Previous'].shift(1)
+df_agg = df_agg.drop(columns=['SHOT_NUMBER', 'FGM', '% Previous'])
 df_final = df_agg.join(df_shift)
-#df_d['% shot_game'] = df_d['FGM'] / df_d['SHOT_NUMBER']
+df_final = pd.merge(df, df_final, on=['player_name', 'GAME_ID'], how='inner')
+df_final.to_csv(r"C:\Users\luca\Documents\GitHub\basket-shots\datasets\shot_logs_wpp.csv")
