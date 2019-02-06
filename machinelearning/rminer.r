db7d4c45c2d776357180be0799a5efc8fe32ebc3
Antonio Vivace <avivace4@gmail.com>
2019-02-06 10:36:28 +0100

    Task to prob so we can generate ROC

--- machinelearning/rminer.r
+++ machinelearning/rminer.r
@@ -29,7 +29,7 @@ dataset = dataset[-c(1, 2)]
 str(dataset)
 
 # Use *only* the first X entries
-dataset = head(dataset, 25000)
+dataset = head(dataset, 2000)
 #dataset$SHOT_CLOCK = as.numeric(as.character(dataset$SHOT_CLOCK))
 
 # Scale
@@ -77,8 +77,9 @@ trainset= allset$train
 testset= allset$test
 
 # Train model
-model=fit(shot_result~.,trainset,model="svm", task="class")
+model=fit(shot_result~.,trainset,model="svm", task="prob")
 I = Importance(model, trainset, method="1D-SA")
+# Importance
 print(I$imp)
 print(round(I$imp,digits=2))
 # Print model parameters
@@ -86,14 +87,12 @@ print(model@mpar)
 prediction = predict(model, testset)
 accuracy=mmetric(testset$shot_result,prediction,metric=c("ACC", "TPR"))
 print(accuracy)
-m=mmetric(testset$shot_result,prediction,metric=c("CONF"))
-print(m$conf) # confusion matrix
+m=mmetric(testset$shot_result,prediction,metric=c("AUC"))
+print(m) # confusion matrix
 
-# LIFT and ROC curves
-txt=paste(levels(trainset$SHOT_RESULT)[2],"AUC:",round(mmetric(trainset$SHOT_RESULT,prediction,metric="AUC",TC=2),2))
-mgraph(trainset$SHOT_RESULT,prediction,graph="ROC",baseline=TRUE,Grid=10,main=txt,TC=2,PDF="roc-1")
-txt=paste(levels(trainset$SHOT_RESULT)[2],"ALIFT:",round(mmetric(trainset$SHOT_RESULT,prediction,metric="ALIFT",TC=2),2))
-mgraph(testset$SHOT_RESULT,prediction,graph="LIFT",baseline=TRUE,Grid=10,main=txt,TC=2,PDF="lift-1")
+# ROC
+txt=paste(levels(testset$shot_result)[2],"AUC:",round(mmetric(testset$shot_result,prediction,metric="AUC",TC=2),2))
+mgraph(testset$shot_result,prediction,graph="ROC",baseline=TRUE,Grid=10,main=txt,TC=2,PDF="roc-1")
 
 # Cross validation
 valdata = crossvaldata(shot_result~., dataset, fit, predict, ngroup = 10, task="class", model="ksvm")
