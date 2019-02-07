d683c0287ff950b254054fc70101c71668142c82
Antonio Vivace <avivace4@gmail.com>
2019-02-07 19:50:47 +0100

    Update build script

--- docs/build.sh
+++ docs/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.log
+rm *.out
+rm *.toc
+
+xelatex -8bit -shell-escape index
 bibtex index
+xelatex -8bit -shell-escape index
 xelatex -8bit -shell-escape index
