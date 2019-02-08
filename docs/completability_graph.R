f7d3758babbc537be3f2cbfd5307fda9557a8e57
Antonio Vivace <avivace4@gmail.com>
2019-02-08 15:43:06 +0100

    Add Completability plot

---
+++ docs/completability_graph.R
@@ -0,0 +1,19 @@
+setwd('~/github/basket-shots/docs/')
+library(ggplot2)
+dataset = read.csv('completeness_over_time.csv', sep=",")
+
+dataset$Date = as.Date(dataset$Date,"%d/%m/%Y %H:%M")
+
+completability = ggplot( data = dataset, aes( Date, Completeness )) + 
+  expand_limits(y = 0) + 
+  geom_line() +
+  geom_ribbon(data = dataset[dataset$Date <= dataset$Date[500],], aes(ymin=0, ymax=Completeness), fill = "gray") +
+  geom_ribbon(data = dataset[dataset$Date >= dataset$Date[500],], aes(ymin=50, ymax=Completeness), fill = "orange") +
+  geom_ribbon(data = dataset[dataset$Date >= dataset$Date[500],], aes(ymin=0, ymax=50), fill = "lightgreen") +
+  annotate("text", label = "A", x = dataset$Date[1100], y = 25, size = 8, colour = "black") +
+  annotate("text", label = "Cb", x = dataset$Date[1100], y = 75, size = 8, colour = "black")
+
+ggsave(file="completability.pdf", plot=completability, width=10, size=20)
+
+# geom_ribbon(aes(ymin=0, ymax=50), fill = "black")
+  
